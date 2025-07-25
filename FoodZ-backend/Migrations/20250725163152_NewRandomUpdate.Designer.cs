053b63debdcb41999b92a6ecdfe10d91c3b3072e
Yash Kiradoo <yash.10836026@ltimindtree.com>
2025-07-25 22:07:00 +0530

    Added new schema changes

--- FoodZ-backend/Migrations/20250725163152_NewRandomUpdate.Designer.cs
+++ FoodZ-backend/Migrations/20250725163152_NewRandomUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Foodz.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FoodZ_backend.Migrations
 {
     [DbContext(typeof(FoodZDbContext))]
-    partial class FoodZDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250725163152_NewRandomUpdate")]
+    partial class NewRandomUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FoodZ-backend/Migrations/20250725163152_NewRandomUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodZ_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewRandomUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
